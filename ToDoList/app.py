52dbb94dd5e28b9ed4d9243c94c37b1c849f0351
Darkay10 <59738162+Darkay10@users.noreply.github.com>
2020-02-14 15:47:36 +0100

    Update app.py

--- ToDoList/app.py
+++ ToDoList/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////c/Users/Jose/Pictures/ToDoList/todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////c/Users/Jose/Pictures/ToDoList/todo.db' # Declaramos donde esta la base de datos
 
 db = SQLAlchemy(app)
 
